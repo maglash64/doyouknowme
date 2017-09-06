4cdac0eba03f63d773c4b0bca8f08df9b2a68169
maglash <maglash64@gmail.com>
2017-09-07 00:19:05 +0530

    updated to dotnet-sdk-2.0.0

---
+++ Models/profiles.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace doyouknowme.Models
+{
+    public class UserProfile
+    {
+        public string uname {get; set;}
+        public string id {get; set;}
+        public string picture {get; set;}
+        public string email {get; set;}
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace doyouknowme
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void ConfigureRoute(IRouteBuilder route)
+        {
+            route.MapRoute("Default","{Controller=base}/{Action=index}/{id?}");
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(ConfigureRoute);
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("<html><body><h1 align='center'>404 : Page Not Found!</h1></body></html>");
+            });
+        }
+    }
+}
